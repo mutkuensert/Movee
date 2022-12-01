cb2d7e286fb585098b0532bf995fc26ea5155c22
M. Utku Ensert <mutkuensert@hotmail.com>
2022-12-01 11:51:49 +0300

    Create MyNavHost

---
+++ app/src/main/java/com/mutkuensert/movee/ui/MyNavHost.kt
@@ -0,0 +1,23 @@
+package com.mutkuensert.movee.ui
+
+import androidx.compose.runtime.Composable
+import androidx.compose.ui.Modifier
+import androidx.navigation.NavHostController
+import androidx.navigation.compose.NavHost
+import androidx.navigation.compose.composable
+import androidx.navigation.compose.rememberNavController
+import com.mutkuensert.movee.ui.home.MoveeHome
+import com.mutkuensert.movee.util.MOVEE_HOME
+
+@Composable
+fun MyNavHost(
+    modifier: Modifier = Modifier,
+    navController: NavHostController = rememberNavController(),
+    startDestination: String = MOVEE_HOME
+) {
+    NavHost(modifier = modifier,
+    navController = navController,
+    startDestination = startDestination){
+        composable(MOVEE_HOME){ MoveeHome()}
+    }
+}
---
+++ app/src/main/java/com/mutkuensert/movee/util/Constants.kt
@@ -0,0 +1,3 @@
+package com.mutkuensert.movee.util
+
+const val MOVEE_HOME = "moveeHome"
