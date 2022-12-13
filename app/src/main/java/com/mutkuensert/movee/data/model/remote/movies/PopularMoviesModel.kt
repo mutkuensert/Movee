9abd9cdd0a6842fe857f027ea45348e84ce73773
M. Utku Ensert <mutkuensert@hotmail.com>
2022-12-13 16:54:35 +0300

    Separate data models

--- app/src/main/java/com/mutkuensert/movee/data/model/remote/movies/PopularMoviesModel.kt
+++ app/src/main/java/com/mutkuensert/movee/data/model/remote/movies/PopularMoviesModel.kt
@@ -8,10 +8,3 @@ data class PopularMoviesModel(
     @Json(name = "total_results") val totalResults: Int,
     @Json(name = "total_pages") val totalPages: Int
 )
-
-data class PopularMoviesResult(
-    @Json(name = "poster_path") val posterPath: String?,
-    val title: String,
-    val id: Int,
-    @Json(name = "vote_average") val voteAverage: Double
-)
