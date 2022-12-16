6653124847228c66bc71f8edb673be720920892d
M. Utku Ensert <mutkuensert@hotmail.com>
2022-12-16 13:39:27 +0300

    Show person cast below person screen

--- app/src/main/java/com/mutkuensert/movee/ui/person/Person.kt
+++ app/src/main/java/com/mutkuensert/movee/ui/person/Person.kt
@@ -1,8 +1,13 @@
 package com.mutkuensert.movee.ui.person
 
+import android.util.Log
 import android.widget.Toast
+import androidx.compose.foundation.clickable
 import androidx.compose.foundation.layout.*
+import androidx.compose.foundation.lazy.LazyColumn
+import androidx.compose.foundation.lazy.items
 import androidx.compose.foundation.rememberScrollState
+import androidx.compose.foundation.shape.RoundedCornerShape
 import androidx.compose.foundation.verticalScroll
 import androidx.compose.material.Card
 import androidx.compose.material.CircularProgressIndicator
@@ -11,9 +16,11 @@ import androidx.compose.runtime.Composable
 import androidx.compose.runtime.State
 import androidx.compose.ui.Alignment
 import androidx.compose.ui.Modifier
+import androidx.compose.ui.draw.clip
 import androidx.compose.ui.graphics.Color
 import androidx.compose.ui.graphics.RectangleShape
 import androidx.compose.ui.layout.ContentScale
+import androidx.compose.ui.platform.LocalConfiguration
 import androidx.compose.ui.platform.LocalContext
 import androidx.compose.ui.text.font.FontWeight
 import androidx.compose.ui.unit.dp
@@ -24,25 +31,36 @@ import androidx.lifecycle.compose.collectAsStateWithLifecycle
 import coil.compose.SubcomposeAsyncImage
 import coil.request.ImageRequest
 import com.mutkuensert.movee.data.model.remote.person.PersonDetailsModel
-import com.mutkuensert.movee.util.IMAGE_BASE_URL
-import com.mutkuensert.movee.util.Resource
-import com.mutkuensert.movee.util.SIZE_ORIGINAL
-import com.mutkuensert.movee.util.Status
+import com.mutkuensert.movee.data.model.remote.person.PersonMovieCastModel
+import com.mutkuensert.movee.util.*
 
+private const val TAG = "Person screen composable"
 
 @OptIn(ExperimentalLifecycleComposeApi::class)
 @Composable
 fun Person(personId: Int?, viewModel: PersonViewModel = hiltViewModel(), navigateToMovieDetails: (movieId: Int) -> Unit){
     val personDetails = viewModel.personDetails.collectAsStateWithLifecycle()
+    val personCast = viewModel.personCast.collectAsStateWithLifecycle()
 
     if(personId != null){
         viewModel.getPersonDetails(personId!!)
-        PersonDetailsDataObserver(data = personDetails)
+        Column(
+            modifier = Modifier
+                .fillMaxWidth()
+                .height(LocalConfiguration.current.screenHeightDp.dp)
+                .verticalScroll(rememberScrollState())
+                .padding(bottom = 25.dp)
+        ){
+            PersonDetailsDataObserver(data = personDetails, loadCastIfSuccessful = { viewModel.getPersonCast(personId=personId!!) })
+            Spacer(modifier = Modifier.height(15.dp))
+            PersonCastDataObserver(data = personCast, modifier = Modifier.height(LocalConfiguration.current.screenHeightDp.dp))
+        }
+
     }
 }
 
 @Composable
-fun PersonDetailsDataObserver(data: State<Resource<PersonDetailsModel>>) {
+fun PersonDetailsDataObserver(data: State<Resource<PersonDetailsModel>>, loadCastIfSuccessful: () -> Unit) {
 
     when (data.value.status) {
         Status.STANDBY -> {}
@@ -65,6 +83,7 @@ fun PersonDetailsDataObserver(data: State<Resource<PersonDetailsModel>>) {
         Status.SUCCESS -> {
             data.value.data?.let { personDetails: PersonDetailsModel ->
                 PersonDetailsItem(personDetails = personDetails)
+                loadCastIfSuccessful()
             }
         }
 
@@ -76,60 +95,141 @@ fun PersonDetailsDataObserver(data: State<Resource<PersonDetailsModel>>) {
 
 @Composable
 fun PersonDetailsItem(personDetails: PersonDetailsModel) {
-    Column(
-        modifier = Modifier
-            .fillMaxSize()
-            .verticalScroll(rememberScrollState())
-            .padding(bottom = 30.dp)
-    ) {
 
-        personDetails.profilePath?.let { posterPathNonNull ->
+    personDetails.profilePath?.let { posterPathNonNull ->
 
-            Card(
-                elevation = 10.dp,
-                shape = RectangleShape
-            ) {
-                SubcomposeAsyncImage(
-                    model = ImageRequest.Builder(LocalContext.current)
-                        .data("$IMAGE_BASE_URL$SIZE_ORIGINAL${posterPathNonNull}")
-                        .crossfade(true)
-                        .build(),
-                    loading = {
-                        Column(
-                            modifier = Modifier.fillMaxSize(),
-                            horizontalAlignment = Alignment.CenterHorizontally
-                        ) {
-                            Spacer(Modifier.height(50.dp))
-                            CircularProgressIndicator(
-                                color = Color.Gray,
-                                modifier = Modifier.size(100.dp)
-                            )
-                            Spacer(Modifier.height(50.dp))
+        Card(
+            elevation = 10.dp,
+            shape = RectangleShape
+        ) {
+            SubcomposeAsyncImage(
+                model = ImageRequest.Builder(LocalContext.current)
+                    .data("$IMAGE_BASE_URL$SIZE_ORIGINAL${posterPathNonNull}")
+                    .crossfade(true)
+                    .build(),
+                loading = {
+                    Column(
+                        modifier = Modifier.fillMaxSize(),
+                        horizontalAlignment = Alignment.CenterHorizontally
+                    ) {
+                        Spacer(Modifier.height(50.dp))
+                        CircularProgressIndicator(
+                            color = Color.Gray,
+                            modifier = Modifier.size(100.dp)
+                        )
+                        Spacer(Modifier.height(50.dp))
+                    }
+                },
+                modifier = Modifier.fillMaxWidth(),
+                contentDescription = "Tv Poster",
+                contentScale = ContentScale.FillWidth
+            )
+        }
+    }
+
+    Column(modifier = Modifier.padding(horizontal = 15.dp)) {
+        Spacer(modifier = Modifier.height(15.dp))
+
+        Text(
+            text = personDetails.name,
+            color = Color.DarkGray,
+            fontWeight = FontWeight.ExtraBold,
+            fontSize = 30.sp
+        )
+
+        Spacer(modifier = Modifier.height(15.dp))
+
+        Text(text = personDetails.biography)
+
+        Spacer(modifier = Modifier.height(15.dp))
+
+    }
+
+}
+
+@Composable
+fun PersonCastDataObserver(data: State<Resource<List<PersonMovieCastModel>>>, modifier: Modifier = Modifier){
+    Column(modifier = modifier) {
+        when (data.value.status) {
+            Status.STANDBY -> {}
+
+            Status.LOADING -> {
+                Column(
+                    modifier = Modifier.fillMaxSize(),
+                    horizontalAlignment = Alignment.CenterHorizontally
+                ) {
+                    Spacer(Modifier.height(50.dp))
+
+                    CircularProgressIndicator(
+                        modifier = Modifier.size(100.dp),
+                        strokeWidth = 6.dp,
+                        color = Color.Gray
+                    )
+                }
+            }
+
+            Status.SUCCESS -> {
+                data.value.data?.let { castList: List<PersonMovieCastModel> ->
+                    LazyColumn {
+                        items(castList){ personMovieCastModel: PersonMovieCastModel ->
+                            PersonCastItem(movie = personMovieCastModel)
                         }
-                    },
-                    modifier = Modifier.fillMaxWidth(),
-                    contentDescription = "Tv Poster",
-                    contentScale = ContentScale.FillWidth
+                    }
+                }
+
+            }
+
+            Status.ERROR -> {
+                Toast.makeText(LocalContext.current, "${data.value.message}", Toast.LENGTH_LONG)
+                    .show()
+            }
+        }
+    }
+
+
+}
+
+@Composable
+fun PersonCastItem(movie: PersonMovieCastModel){
+    Log.i(TAG, "PopularMoviesItem composable image url: $IMAGE_BASE_URL$POSTER_SIZE_W500${movie.posterPath}")
+
+    Row(modifier = Modifier.padding(vertical = 10.dp, horizontal = 7.dp)) {
+        Card(elevation = 10.dp, modifier = Modifier
+            .fillMaxWidth()
+            .clickable { }) {
+
+            Row(
+                modifier = Modifier.padding(10.dp),
+                verticalAlignment = Alignment.CenterVertically
+            ) {
+
+                Card(elevation = 10.dp) {
+                    SubcomposeAsyncImage(
+                        model = ImageRequest.Builder(LocalContext.current)
+                            .data("$IMAGE_BASE_URL$SIZE_ORIGINAL${movie.posterPath}")
+                            .crossfade(true)
+                            .build(),
+                        loading = {
+                            CircularProgressIndicator(color = Color.Gray, modifier = Modifier.size(150.dp))
+                        },
+                        modifier = Modifier
+                            .clip(RoundedCornerShape(5.dp))
+                            .width(150.dp),
+                        contentDescription = "Movie Poster"
+                    )
+                }
+
+                Spacer(Modifier.width(20.dp))
+
+                Text(
+                    text = movie.title,
+                    color = Color.DarkGray,
+                    fontWeight = FontWeight.ExtraBold,
+                    fontSize = 20.sp
                 )
+
             }
         }
 
-        Column(modifier = Modifier.padding(horizontal = 15.dp)) {
-            Spacer(modifier = Modifier.height(15.dp))
-
-            Text(
-                text = personDetails.name,
-                color = Color.DarkGray,
-                fontWeight = FontWeight.ExtraBold,
-                fontSize = 30.sp
-            )
-
-            Spacer(modifier = Modifier.height(15.dp))
-
-            Text(text = personDetails.biography)
-
-            Spacer(modifier = Modifier.height(15.dp))
-
-        }
     }
 }
--- app/src/main/java/com/mutkuensert/movee/ui/person/PersonViewModel.kt
+++ app/src/main/java/com/mutkuensert/movee/ui/person/PersonViewModel.kt
@@ -5,6 +5,7 @@ import androidx.lifecycle.ViewModel
 import androidx.lifecycle.viewModelScope
 import com.mutkuensert.movee.data.api.PersonApi
 import com.mutkuensert.movee.data.model.remote.person.PersonDetailsModel
+import com.mutkuensert.movee.data.model.remote.person.PersonMovieCastModel
 import com.mutkuensert.movee.util.Resource
 import dagger.hilt.android.lifecycle.HiltViewModel
 import kotlinx.coroutines.Dispatchers
@@ -19,8 +20,11 @@ private const val TAG = "PersonViewModel"
 class PersonViewModel @Inject constructor(private val personApi: PersonApi): ViewModel() {
     private val _personDetails: MutableStateFlow<Resource<PersonDetailsModel>> = MutableStateFlow(
         Resource.standby(null))
-
     val personDetails: StateFlow<Resource<PersonDetailsModel>> get() = _personDetails
+
+    private val _personCast: MutableStateFlow<Resource<List<PersonMovieCastModel>>> = MutableStateFlow(
+        Resource.standby(null))
+    val personCast: StateFlow<Resource<List<PersonMovieCastModel>>> get() = _personCast
 
     fun getPersonDetails(personId: Int){
         viewModelScope.launch(Dispatchers.IO) {
@@ -32,6 +36,21 @@ class PersonViewModel @Inject constructor(private val personApi: PersonApi): Vie
                 _personDetails.value = Resource.success(response.body()!!)
             }else {
                 _personDetails.value = Resource.error("Unsuccessful Person Details Request", null)
+                Log.e(TAG, "Is response successful? ${response.isSuccessful}, Is response body null? ${response.body()}")
+            }
+        }
+    }
+
+    fun getPersonCast(personId: Int){
+        viewModelScope.launch(Dispatchers.IO) {
+            _personCast.value = Resource.loading(null)
+
+            val response = personApi.getPersonMovieCredits(personId = personId)
+
+            if(response.isSuccessful && response.body() != null){
+                _personCast.value = Resource.success(response.body()!!.cast)
+            }else {
+                _personCast.value = Resource.error("Unsuccessful Person Cast Request", null)
                 Log.e(TAG, "Is response successful? ${response.isSuccessful}, Is response body null? ${response.body()}")
             }
         }
