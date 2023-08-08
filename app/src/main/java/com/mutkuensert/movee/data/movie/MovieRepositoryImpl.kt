8e25528dafc078f7e5c43c1e0cd5403e94a22046
M. Utku Ensert <mutkuensert@hotmail.com>
2023-08-09 00:00:23 +0300

    Update MovieRepositoryImpl.kt

--- app/src/main/java/com/mutkuensert/movee/data/movie/MovieRepositoryImpl.kt
+++ app/src/main/java/com/mutkuensert/movee/data/movie/MovieRepositoryImpl.kt
@@ -4,20 +4,16 @@ import androidx.paging.ExperimentalPagingApi
 import androidx.paging.Pager
 import androidx.paging.PagingConfig
 import androidx.paging.PagingData
-import com.mutkuensert.movee.data.account.local.AccountDao
 import com.mutkuensert.movee.data.movie.local.MovieDao
 import com.mutkuensert.movee.data.movie.local.entity.PopularMovieEntity
 import com.mutkuensert.movee.data.movie.remote.mediator.PopularMoviesRemoteMediator
 import com.mutkuensert.movee.domain.movie.MovieRepository
-import com.mutkuensert.movee.library.session.SessionManager
 import javax.inject.Inject
 import kotlinx.coroutines.flow.Flow
 
 class MovieRepositoryImpl @Inject constructor(
     private val movieApi: MovieApi,
     private val movieDao: MovieDao,
-    private val accountDao: AccountDao,
-    private val sessionManager: SessionManager,
 ) : MovieRepository {
 
     @OptIn(ExperimentalPagingApi::class)
