0f3d8a69abcf8ceb9e11e253793407d1c9a399f8
M. Utku Ensert <mutkuensert@hotmail.com>
2024-02-12 21:21:19 +0300

    Remove showToastIfNotNull

--- presentation/src/main/java/movee/presentation/core/Context.kt
+++ presentation/src/main/java/movee/presentation/core/Context.kt
@@ -24,12 +24,6 @@ fun Context.showToast(text: String, duration: Int = Toast.LENGTH_SHORT) {
     Toast.makeText(this, text, duration).show()
 }
 
-fun Context.showToastIfNotNull(text: String?, duration: Int = Toast.LENGTH_SHORT) {
-    if (text != null) {
-        Toast.makeText(this, text, duration).show()
-    }
-}
-
 fun Context.getInsetsController(): WindowInsetsControllerCompat? {
     if (asActivity() == null) return null
 
